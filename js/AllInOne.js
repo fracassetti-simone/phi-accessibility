c79b5c7ededb1868575ea514d05f88760fcf1911
fracassetti-simone <simone@phi.it>
2025-07-09 12:21:23 +0200

    v1.1.1

--- js/AllInOne.js
+++ js/AllInOne.js
@@ -269,7 +269,10 @@ window.Widget = class{
         // infine monta nella finestra del tuo widget
         this.#container.popup.appendChild(grid);
 
+        const credits = document.createElement('widget-credits');
+        credits.innerHTML = 'Widget sviluppato da <a href="https://phi.it" target="_blank">PHI Informatica</a>';
 
+        this.#container.popup.appendChild(credits);
         this.#container.appendChild(this.#container.popup);
     }
 
@@ -309,3 +312,40 @@ window.Accessibility = class{
         return rgb.map(i => 255 - i);
     }
 }
+
+const accessibility = new window.Accessibility({
+    style: {
+        variables: {
+            iconSize: '3em',
+            
+            widgetWidth: '30em',
+            widgetSpacing: '2em',
+            widgetPadding: '1em',
+            widgetTextColor: '#000',
+            widgetColor: '#000',
+            widgetIconColor: '#fff',
+            widgetBackgroundColor: '#fff'/* '#eee' */,
+            widgetIconBackgroundColor: '#0066ff',
+            widgetBorder: '0'/* '2px solid #aaa' */,
+            widgetShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
+
+            widgetButtonsSpacing: '.5em .5em',
+            widgetButtonIconSize: '2em',
+            widgetButtonsBorder: '1px solid transparent'/* '1px solid #aaa' */,
+            widgetButtonsBackgroundColor: '#fff',
+            widgetButtonsTextColor: '#000',
+            widgetButtonsFontWeight: '500',
+
+            widgetButtonsToggledBackgroundColor: '#fff',
+            widgetButtonsToggledTextColor: '#0066ff',
+            widgetButtonsToggledBorder: '2px solid #0066ff',
+            widgetButtonsToggledFontWeight: '700',
+            widgetButtonsShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
+
+            widgetHeaderColor: '#0066ff',
+            widgetHeaderTextColor: '#fff'
+        }
+    }
+});
+
+accessibility.widget.show();
